6a8c27353636442ac5c0fc3c4a61472ea5580927
EricJYE <ericisawesome2019@gmail.com>
2026-05-15 10:50:58 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'color-story-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])).then(() => self.skipWaiting()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
